e5a9e7358e3c8974a2e55bebc98aeb6da1136d51
ankitakonale <54102128+ankitakonale@users.noreply.github.com>
2023-12-17 15:30:13 +0530

    Create index.js

---
+++ JavaScriptFilterFunction/index.js
@@ -0,0 +1,8 @@
+// Add input element inside form, before button, to take fruit description
+
+
+// Show the fruit description in italics on the next line
+
+
+// Create a filter that shows only those fruits whose either name or description or both matches the entered text
+
